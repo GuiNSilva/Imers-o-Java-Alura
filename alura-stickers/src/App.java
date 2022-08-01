b307378daa01e21a615a2fe3b7e997c83fbe7cc8
guilherme <guigarks@gmail.com>
2022-08-01 19:26:05 -0300

    Imersão Alura - aula 3 -Refatoracao e OO

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,11 +1,6 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.util.List;
+ import java.util.List;
 import java.util.Map;
 
 public class App {
@@ -18,35 +13,50 @@ public class App {
 
         //Criar um conexão http para buscar os top 250 filmes
         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
-        URI endereco = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request =  HttpRequest.newBuilder(endereco).GET().build(); 
-        HttpResponse<String> response =client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-        System.out.println(body);
+        ExtratorDeConteudo extrator = new ExtratorConteudoImdb();
+        
+        
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        //ExtratorDeConteudo extrator = new ExtratorConteudoNasa();
+       
+       
+       
+        var http = new ClienteHttp();
+        String json =   http.buscaDados(url);
+       
 
         //Buscar informções relevantes(título, poster,classificação)
 
-        JsonParser parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+      
        // System.out.println(listaDeFilmes.size()); 
         //System.out.println(listaDeFilmes.get(0));
 
 
         //Exibir e manipular os dados de acordo com a necessidade.
+       
+       
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+       
         var stickMoviesGenerator = new stickMoviesGenerator();
-        for (Map<String,String> filme : listaDeFilmes) {
+        
+        
+        for (int i  = 0 ;i <3; i++) {
 
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
-            String nomeArquivo = titulo + ".png";
+            Conteudo conteudo = conteudos.get(i);
 
-            InputStream inputStream = new URL(urlImagem).openStream();
+ 
+           
+           
+           
+            
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
+
 
             stickMoviesGenerator.cria(inputStream, nomeArquivo);
 
 
-            System.out.println(filme.get("title"));
+            System.out.println(conteudo.getTitulo());
             System.out.println();
 
 
